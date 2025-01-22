80c3171f39c7f7d042562b17b9af79aad1780fbd
sugimotoshoki4959 <sugimoto.shoki.4959@members.co.jp>
2025-01-22 18:28:51 +0900

    アニメーションもう少し

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,25 @@ $(function() {
 
     // console.log('test');
 
+    /* slick */
+    $(".ks-slick").slick({
+        autoplay: true, // 自動再生ON
+        fade: true, // フェードON
+        dots: false, // ドットOFF
+        arrows: false, // 矢印OFF
+        speed: 3000, // スライド、フェードアニメーションの速度2000ミリ秒
+        autoplaySpeed: 3000, // 自動再生速度4000ミリ秒
+        pauseOnFocus: false, // フォーカスで一時停止OFF
+        pauseOnHover: false // マウスホバーで一時停止OFF
+    });
+
     /* スクロールアニメーション */
     $(window).on('scroll', function(){
 
         $('.slideIn-up').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 200;
+            let offset = 100;
             if (scrollPos > targetOffset + offset) {
                 $(this).addClass('slideIn-up-on');
             }
@@ -17,7 +29,7 @@ $(function() {
         $('.fadeIn-about').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 300;
+            let offset = 200;
             if (scrollPos > targetOffset + offset) {
                 $(this).addClass('fadeIn-about-on');
                 $(this).parent('.ks-about-flex').find('h3.fadeIn-about').addClass('fadeIn-about-on');
@@ -25,20 +37,33 @@ $(function() {
             }
         });
 
-
         $('.fadeIn').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 200;
+            let offset = 100;
             if (scrollPos > targetOffset + offset) {
                 $(this).addClass('fadeIn-on');
+            }
+        });
+
+        $('.ks-pickUp-item_img-obi').each(function() {
+            let targetOffset = $(this).offset().top;
+            let scrollPos = $(window).scrollTop() + $(window).height();
+            let offset = 100;
+            if (scrollPos > targetOffset + offset) {
+                $(this).find('.ks-pickUp-item_img-obi-bg-sp').addClass('ks-pickUp-item_img-obi-bg-sp-on');
+                $(this).find('.ks-pickUp-item_img-obi-bg-sp div').addClass('ks-pickUp-item_img-obi-bg-sp-on');
+                $(this).find('.ks-pickUp-item_img-obi-bg-pc').addClass('ks-pickUp-item_img-obi-bg-pc-on');
+                $(this).find('.ks-pickUp-item_img-obi-bg-pc div').addClass('ks-pickUp-item_img-obi-bg-pc-on');
+                $(this).find('p').addClass('ks-pickUp-item_img-obi_p-on');
+                $(this).find('p span').addClass('ks-pickUp-item_img-obi_span-on');
             }
         });
 
         $('.slideIn-left').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 200;
+            let offset = 100;
             if (scrollPos > targetOffset + offset) {
                 $(this).addClass('slideIn-left-on');
             }
@@ -47,9 +72,20 @@ $(function() {
         $('.slideIn-up-pickUp').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 200;
+            let offset = 100;
             if (scrollPos > targetOffset + offset) {
                 $(this).addClass('slideIn-up-pickUp-on');
+            }
+        });
+
+        $('.fadeIn-ranking').each(function() {
+            let targetOffset = $(this).offset().top;
+            let scrollPos = $(window).scrollTop() + $(window).height();
+            let offset = 100;
+            if (scrollPos > targetOffset + offset) {
+                $(this).addClass('fadeIn-ranking-on');
+                $(this).parent('.ks-ranking').find('.slideIn-left-ranking').addClass('slideIn-left-ranking-on');
+                $(this).parent('.ks-recommend').find('.slideIn-left-ranking').addClass('slideIn-left-ranking-on');
             }
         });
     });
